e3898e5e1cc16353cead65c97415164f93970da2
AshenOneOrz <ashneoneorz@gmail.com>
2021-10-15 17:39:30 +0800

    Version: 1.0.0

--- index.js
+++ index.js
@@ -1,57 +1,71 @@
-const { log, readFile, writeFile, replaceAll } = require('./lib/utils')
+const { log, readFile, writeFile, appendFile, replaceAll } = require('./lib/utils')
 
 // const request = require('sync-request')
 // const request = require('request')
 // const Agent = require('socks5-http-client/lib/Agent')
 
-const axios = require('axios')
+const Axios = require('axios')
+const HttpsProxyAgent = require('https-proxy-agent')
+const iconvLite = require('iconv-lite')
+
+const httpsAgent = new HttpsProxyAgent(`http://127.0.0.1:7890`)
+const axios = Axios.create({
+    proxy: false,
+    httpsAgent,
+})
 
 const parseHtml = (html) => {
     let lines = html.split('\n')
-
     for (let i = 1; i < lines.length - 1; i++) {
         let line = lines[i]
         let prevLine = lines[i - 1]
         let nextLine = lines[i + 1]
 
         if (prevLine.startsWith('<br>') && nextLine.startsWith('</div>')) {
-            let data = line
-
-            replaceAll(data, '<br />', '\r\n')
-            replaceAll(data, '&nbsp;', ' ')
-
+            let data = replaceAll(line, '<br />', '\r\n')
+            data = replaceAll(data, '&nbsp;', '')
+            data = replaceAll(data, '&emsp;', '')
             return data
         }
     }
 }
 
+const WriteChaptersToFile = (contents) => {
+    log('下载完毕，开始写入')
+    for (let i = 0; i < contents.length; i++) {
+        const c = contents[i]
+        let path = `./cache/${c.path}`
+
+        let file = readFile(path)
+        let title = `${c.title}\r\n\r\n`
+        let data = title + parseHtml(file)
+        appendFile('芝加哥1990.txt', data)
+    }
+}
+
 // contents 是[{title: '第一章', path: '1.html'}, ...] 格式的目录
 // baseUrl 是 https://xiaoshuo.com/name/ 格式的 url， 用这个 url 和每个章节的 path 进行拼接即可得出完整的 url
-const getPages = (contents, baseUrl) => {
+const downloadPages = async (contents, baseUrl) => {
+    let cs = JSON.parse(JSON.stringify(contents))
+    log('开始下载 html 文件')
     for (const content of contents) {
         let url = baseUrl + content.path
-        url = 'https://www.baidu.com/'
-
-        let proxy = {
-            host: '127.0.0.1',
-            port: 7890,
+        try {
+            let response = await axios.get(url, {
+                responseType: 'arraybuffer',
+            })
+            let data = iconvLite.decode(response.data, 'gbk').toString()
+            writeFile(`./cache/${content.path.split('.')[0]}.html`, data)
+            cs.shift()
+        } catch (error) {
+            log('error', content)
         }
+    }
 
-        axios({
-            method: 'get',
-            url,
-            proxy: proxy,
-        })
-            .then(function (response) {
-                log('------------Success')
-            })
-            .catch(function (error) {
-                log('------------Error')
-                log(error)
-                log('------------Error')
-            })
-
-        break
+    if (cs.length !== 0) {
+        downloadPages(cs, baseUrl)
+    } else {
+        WriteChaptersToFile(contents)
     }
 }
 
@@ -59,7 +73,7 @@ const parseContents = (url) => {
     let html = readFile(url)
     let lines = html.split('\n')
     let contents = []
-
+    log('开始解析目录')
     for (const line of lines) {
         if (line.startsWith('<li>')) {
             let s1 = line.indexOf('href="') + 6
@@ -89,9 +103,8 @@ const __main = () => {
     let contents = parseContents(url)
 
     let baseUrl = `https://www.ptwxz.com/html/10/10125/`
-    let pages = getPages(contents, baseUrl)
+    let pages = downloadPages(contents, baseUrl)
 
-    // let  = parseHtml(url)
 }
 
 __main()
